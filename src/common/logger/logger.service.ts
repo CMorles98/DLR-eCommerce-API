e5cbeb3621155df92e5bec45c7e7321ed12079da
CMorles98 <christopher.mh98@gmail.com>
2024-04-01 10:19:39 -0400

    remove cs from git

--- src/common/logger/logger.service.ts
+++ src/common/logger/logger.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@nestjs/common'
 import { logger } from './winston.config'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class LoggerService {
+  constructor(private readonly configService: ConfigService) {}
+
+  private connectionString = this.configService.getOrThrow('MONGO_URL')
+
   log(message: string, context?: string) {
-    logger.info(message, { context })
+    logger(this.connectionString).info(message, { context })
   }
 
   error(message: string, trace: string, context?: string) {
-    logger.error(message, { context, trace })
+    logger(this.connectionString).error(message, { context, trace })
   }
 
   warn(message: string, context?: string) {
-    logger.warn(message, { context })
+    logger(this.connectionString).warn(message, { context })
   }
 
   debug(message: string, context?: string) {
-    logger.debug(message, { context })
+    logger(this.connectionString).debug(message, { context })
   }
 }
--- src/common/logger/winston.config.ts
+++ src/common/logger/winston.config.ts
@@ -2,45 +2,51 @@ import * as winston from 'winston'
 import 'winston-daily-rotate-file'
 import * as winstonMongoDB from 'winston-mongodb'
 
-const transports = [
-  new winston.transports.Console({
-    format: winston.format.combine(
-      winston.format.timestamp(),
-      winston.format.colorize(),
-      winston.format.printf(({ timestamp, level, message, context, trace }) => {
-        return `${timestamp} [${context}] ${level}: ${message}${trace ? `\n${trace}` : ''}`
-      }),
-    ),
-  }),
-  new winston.transports.DailyRotateFile({
-    filename: 'logs/dlr-%DATE%.log',
-    datePattern: 'YYYY-MM-DD',
-    zippedArchive: true,
-    maxSize: '20m',
-    maxFiles: '14d',
-    format: winston.format.combine(
-      winston.format.timestamp(),
-      winston.format.json(),
-    ),
-  }),
-  new winstonMongoDB.MongoDB({
-    level: 'info',
-    db: 'mongodb+srv://DLR:D3aJ1fYxWgIAuyLz@dlr-cluster.gkrigla.mongodb.net/?retryWrites=true&w=majority&appName=DLR-Cluster',
-    options: {
-      useUnifiedTopology: true,
-    },
-    collection: 'logs',
-    format: winston.format.combine(
-      winston.format.timestamp(),
-      winston.format.json(),
-    ),
-    dbName: 'DLR',
-  }),
-]
+const transports = (connectionString: string) => {
+  return [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.colorize(),
+        winston.format.printf(
+          ({ timestamp, level, message, context, trace }) => {
+            return `${timestamp} [${context}] ${level}: ${message}${trace ? `\n${trace}` : ''}`
+          },
+        ),
+      ),
+    }),
+    new winston.transports.DailyRotateFile({
+      filename: 'logs/dlr-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+    }),
+    new winstonMongoDB.MongoDB({
+      level: 'info',
+      db: connectionString,
+      options: {
+        useUnifiedTopology: true,
+      },
+      collection: 'logs',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+      dbName: 'DLR',
+    }),
+  ]
+}
 
-export const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports,
-  handleExceptions: true,
-})
+export const logger = (connectionString: string) => {
+  return winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: transports(connectionString),
+    handleExceptions: true,
+  })
+}
